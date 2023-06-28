67e544a71a4770f06e8042601b2136fe96f3317d
FabrizioFavia <personal.favia@gmail.com>
2023-06-28 09:28:34 +0200

    added items to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,82 @@
-<footer class="p-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</footer>
+<footer>
+
+    <div class="topFooter">
+        <img class="w-100" src="{{ Vite::asset('resources/img/footer-bg.jpg') }}" alt="">
+        <div class="container-lg h-100">
+            <div class="topFooter">
+                <div class="container-lg">
+                    <div class="row justify-content-evenly">
+                        <div class="col-6">
+                            <div class="row pt-5">
+                                <div class="col-3">
+                                    <p class="text-white title fs-4">DC COMICS</p>
+                                    <ul>
+                                        <li><a href="#">Characters</a></li>
+                                        <li><a href="#">Comics</a></li>
+                                        <li><a href="#">Movies</a></li>
+                                        <li><a href="#">TV</a></li>
+                                        <li><a href="#">Games</a></li>
+                                        <li><a href="#">Videos</a></li>
+                                        <li><a href="#">News</a></li>
+                                    </ul>
+
+                                    <p class="text-white title fs-4">SHOP</p>
+                                    <ul>
+                                        <li><a href="#">Shop DC</a></li>
+                                        <li><a href="#">Shop DC Collectibles</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-3">
+                                    <p class="text-white title fs-4">DC</p>
+                                    <ul>
+                                        <li><a href="#">Terms Of Use</a></li>
+                                        <li><a href="#">Privacy policy (New)</a></li>
+                                        <li><a href="#">Ad Choices</a></li>
+                                        <li><a href="#">Advertising</a></li>
+                                        <li><a href="#">Jobs</a></li>
+                                        <li><a href="#">Subscriptions</a></li>
+                                        <li><a href="#">CPSC Certificates</a></li>
+                                        <li><a href="#">Ratings</a></li>
+                                        <li><a href="#">Shop Help</a></li>
+                                        <li><a href="#">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-3">
+                                    <p class="text-white title fs-4">SITES</p>
+                                    <ul>
+                                        <li><a href="#">DC</a></li>
+                                        <li><a href="#">MAD Magazine</a></li>
+                                        <li><a href="#">DC kids</a></li>
+                                        <li><a href="#">DC Universe</a></li>
+                                        <li><a href="#">DC Power Visa</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 logoContainer">
+                            <img src="/assets/dc-logo-bg.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottomFooter d-flex justify-content-between align-items-center">
+        <div class="container-lg d-flex justify-content-between align-items-center">
+            <div class="leftPart">
+                <button class="text-white px-3 py-2">SIGN-UP NOW!</button>
+            </div>
+            <div class="RightPart d-flex justify-content-between align-items-center">
+                <h4 class="text-primary mt-2 me-3"><a href="">FOLLOW US</a></h4>
+                <img class="ms-3" src="/assets/footer-facebook.png" alt="">
+                <img class="ms-3" src="/assets/footer-twitter.png" alt="">
+                <img class="ms-3" src="/assets/footer-youtube.png" alt="">
+                <img class="ms-3" src="/assets/footer-pinterest.png" alt="">
+                <img class="ms-3" src="/assets/footer-periscope.png" alt="">
+
+
+            </div>
+        </div>
+    </div>
+
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,14 +11,42 @@
             <h3>CURRENT SERIES</h3>
         </div>
         <div class="container-lg comicsContainer">
-
             @foreach ($data as $comic)
                 <div class="card mb-5">
                     <img class="thumb" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" />
                     <p class="text-white">{{ strtoupper($comic['title']) }}</p>
                 </div>
             @endforeach
-
+        </div>
+        <div class="container-lg d-flex justify-content-center">
+            <div class="loadMore text-white bg-primary">
+                <p class="fs-6">LOAD MORE</p>
+            </div>
+        </div>
+        <div class="midNav bg-primary w-100">
+            <div class="container-lg d-flex justify-content-center align-items-center">
+                <div class="midContainer d-flex justify-content-center align-items-center me-5">
+                    <img class="midItem me-3" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <p class="text-white mt-3">DIGITAL COMICS</p>
+                </div>
+                <div class="midContainer d-flex justify-content-center align-items-center me-5">
+                    <img class="midItem me-3" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <p class="text-white mt-3">DIGITAL COMICS</p>
+                </div>
+                <div class="midContainer d-flex justify-content-center align-items-center me-5">
+                    <img class="midItem me-3" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <p class="text-white mt-3">DIGITAL COMICS</p>
+                </div>
+                <div class="midContainer d-flex justify-content-center align-items-center me-5">
+                    <img class="midItem me-3" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <p class="text-white mt-3">DIGITAL COMICS</p>
+                </div>
+                <div class="midContainer d-flex justify-content-center align-items-center me-5">
+                    <img class="midItem me-3" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <p class="text-white mt-3">DIGITAL COMICS</p>
+                </div>
+                
+            </div>
         </div>
     </div>
 @endsection
